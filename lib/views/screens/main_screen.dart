501a9ba2f6587998fc057ac7a4826fc11459d1e0
nuzlee <nuzli.opis@gmail.com>
2025-08-19 14:44:19 +0800

    update

---
+++ lib/views/screens/main_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:simple_app/utils/components/custom_navbar.dart';
+import 'package:simple_app/views/screens/form_page.dart';
+import 'package:simple_app/views/screens/home.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentIndex = 0;
+  List<Widget> screens = [Home(), FormPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: CustomNavbar(),
+    );
+  }
+}
